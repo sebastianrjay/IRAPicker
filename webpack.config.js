bd8cc85387eabba9bbf0a63619113896e9793f5b
sebastianrjay <sebastianrjay@gmail.com>
2017-05-04 16:53:21 -0700

    Initial React environment setup, with yarn pkg manager

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: './lib/entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+}
