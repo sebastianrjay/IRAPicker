544c87e4a408f234f63e7e98e4b1a7ff2855ac1b
sebastianrjay <sebastianrjay@gmail.com>
2018-01-31 23:02:23 -0800

    Calculate before-tax income in optimal runtime with approximated upper bound

--- lib/components/partials/summary.jsx
+++ lib/components/partials/summary.jsx
@@ -3,11 +3,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {
   accountBalanceAtRetirement,
+  afterTaxIncome,
+  beforeTaxIncome,
   taxRefundFromIRADeduction,
 } from '../../util/tax_calculations'
 
 const Summary = (props) => {
   const iraTaxRefund = taxRefundFromIRADeduction(props)
+  const traditionalIRAWithdrawal =
+    beforeTaxIncome(merge({ afterTaxIncome: props.retirementIncome }, props))
   const rothTotalSavings = accountBalanceAtRetirement(props)
   const traditionalIRATotalSavings = 
     accountBalanceAtRetirement(merge({ iraTaxRefund }, props))
@@ -26,6 +30,14 @@ const Summary = (props) => {
           </p>
           <p>{`Roth IRA: $${rothTotalSavings}`}</p>
           <p>{`Traditional IRA: $${traditionalIRATotalSavings}`}</p>
+          <p>
+            {
+              `To earn a post-tax retirement income of $${props.retirementIncome}, 
+              you will need to annually withdraw $${props.retirementIncome} from 
+              your Roth IRA, or $${traditionalIRAWithdrawal} from your traditional 
+              IRA.`
+            }
+          </p>
         </div>
       </div>
     )
