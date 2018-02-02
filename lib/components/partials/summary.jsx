52d6c1d039f2ace788edeb6149dfb9d129e9b977
sebastianrjay <sebastianrjay@gmail.com>
2018-02-01 23:17:00 -0800

    Accurately calculate before-tax income with suboptimal runtime

--- lib/components/partials/summary.jsx
+++ lib/components/partials/summary.jsx
@@ -1,4 +1,5 @@
 import merge from 'lodash/merge'
+import omit from 'lodash/omit'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {
@@ -9,39 +10,40 @@ import {
 } from '../../util/tax_calculations'
 
 const Summary = (props) => {
+  if (!props.annualIncome || !props.retirementIncome) return null
+
   const iraTaxRefund = taxRefundFromIRADeduction(props)
-  const traditionalIRAWithdrawal =
-    beforeTaxIncome(merge({ afterTaxIncome: props.retirementIncome }, props))
+  const beforeTaxIncomeProps =
+    merge({ annualIncome: props.retirementIncome }, omit(props, ['annualIncome']))
+  const traditionalIRAWithdrawal = beforeTaxIncome(beforeTaxIncomeProps)
   const rothTotalSavings = accountBalanceAtRetirement(props)
   const traditionalIRATotalSavings = 
     accountBalanceAtRetirement(merge({ iraTaxRefund }, props))
 
-  if (props.annualIncome && props.retirementIncome) {
-    return (
-      <div className="row">
-        <div className="col-md-12">
-          <p>
-            {
-              `If you continue annually investing $${props.combinedContribution} 
-              in one IRA account and investing all tax refunds from traditional 
-              IRA contributions in other assets, your IRA account balance will 
-              be one of the following when you retire:`
-            }
-          </p>
-          <p>{`Roth IRA: $${rothTotalSavings}`}</p>
-          <p>{`Traditional IRA: $${traditionalIRATotalSavings}`}</p>
-          <p>
-            {
-              `To earn a post-tax retirement income of $${props.retirementIncome}, 
-              you will need to annually withdraw $${props.retirementIncome} from 
-              your Roth IRA, or $${traditionalIRAWithdrawal} from your traditional 
-              IRA.`
-            }
-          </p>
-        </div>
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <p>
+          {
+            `If you continue annually investing $${props.combinedContribution} 
+            in one IRA account and investing all tax refunds from traditional 
+            IRA contributions in other assets, your IRA account balance will 
+            be one of the following when you retire:`
+          }
+        </p>
+        <p>{`Roth IRA: $${rothTotalSavings}`}</p>
+        <p>{`Traditional IRA: $${traditionalIRATotalSavings}`}</p>
+        <p>
+          {
+            `To earn a post-tax retirement income of $${props.retirementIncome}, 
+            you will need to annually withdraw $${props.retirementIncome} from 
+            your Roth IRA, or $${traditionalIRAWithdrawal} from your traditional 
+            IRA.`
+          }
+        </p>
       </div>
-    )
-  } else return null
+    </div>
+  )
 }
 
 export default Summary
