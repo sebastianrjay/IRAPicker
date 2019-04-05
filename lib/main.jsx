a5c29e3c3780dbb196672aa2627dcd88af3446fb
sebastianrjay <sebastianrjay@gmail.com>
2019-04-05 00:11:51 -0400

    Add Google AdSense

--- lib/main.jsx
+++ lib/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { GOOGLE_ADSENSE_API_KEY } from '../config/secrets'
 import store from './store/store'
 import App from './components/app'
+
+(adsbygoogle = window.adsbygoogle || []).push({
+  google_ad_client: GOOGLE_ADSENSE_API_KEY,
+  enable_page_level_ads: true
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root')
